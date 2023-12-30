5a3375df5e0b0cbc7cf1a75f8e757990905ecf0a
Shiv7038 <shivajipawar28000@gmail.com>
2023-12-30 21:17:29 +0530

    test

--- src/test/java/aa.java
+++ src/test/java/aa.java
@@ -1,5 +1,19 @@
 public class aa {
     public static void main(String[] args) {
-        System.out.println("hii");
+
+        System.out.println("Hi Priya");
+        String str="Process finished with exit code";
+        String[] ar=str.split(" ");
+        String revst="";
+        for(int i=0; i<ar.length;i++){
+            String word=ar[i];
+            char []ch=word.toCharArray();
+            String rev="";
+            for(int j=ch.length-1;j>=0;j--){
+                rev=rev+ch[j];
+            }
+            revst=revst+rev+" ";
+        }
+        System.out.println(revst);
     }
 }
